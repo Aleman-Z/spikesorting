24ff71d45cb7838c917a4a914a79ea5859c0a56d
Aleman-Z <jraleman10@gmail.com>
2020-11-13 18:10:37 +0100

    fix_channel_name

--- fix_channel_name.py
+++ fix_channel_name.py
@@ -6,32 +6,38 @@ Created on Sun Oct 18 16:43:45 2020
 @author: adrian
 """
 import os
+import sys
 
-folderpath='/home/adrian/Documents/SpikeSorting/Adrian_test_data/Irene_data/test_without_zero_main_channels/Tetrode_9';
+folderpath=sys.argv[1];
+os.chdir(folderpath)
 
-
-
+#Find .continuous files
 source = '100';
 chprefix = 'CH';
-session = '_0'   ;
 
-[source + '_'+chprefix + x + '.continuous' for x in map(str,channels)]
-
-
-channels=op._get_sorted_channels(folderpath, chprefix, session,9);
-
-
-
-os.listdir(folderpath)
 Files = [f for f in os.listdir(folderpath) if '.continuous' in f
                                                and '_'+chprefix in f
                                                and source in f]
+#Previous name
+Files = [f for f in Files if len(f.split('_')) == 3]
+#New name
+NF=[f.split('_0')[0]+f.split('_0')[1] for f in Files];
+#Rename
+for (f,nf) in zip(Files,NF):
+        print(f, nf)
+        os.rename(f, nf)
 
-if session == '0':
-    Files = [f for f in Files if len(f.split('_')) == 3]
-    NF=[f.split('_0')[0]+f.split('_0')[1] for f in Files];
-    for (f,nf) in zip(Files,NF):
-            print(f, nf)
-            os.rename(f, nf)
+#Aux channels
+chprefix = 'AUX';
 
-  
+Files = [f for f in os.listdir(folderpath) if '.continuous' in f
+                                               and '_'+chprefix in f
+                                               and source in f]
+#Previous name
+Files = [f for f in Files if len(f.split('_')) == 3]
+#New name
+NF=[f.split('_0')[0]+f.split('_0')[1] for f in Files];
+#Rename
+for (f,nf) in zip(Files,NF):
+        print(f, nf)
+        os.rename(f, nf)
