9e7995d11457172e19e12061971dc83f952833f9
Aleman-Z <jraleman10@gmail.com>
2020-11-24 17:02:19 +0100

    phy_save_consensus_post_curation

--- consensus_post_curation.py
+++ consensus_post_curation.py
@@ -26,6 +26,34 @@ import csv
 recording_folder=sys.argv[1];
 os.chdir(recording_folder)
 
+recording = se.OpenEphysRecordingExtractor(recording_folder) 
+channel_ids = recording.get_channel_ids() 
+fs = recording.get_sampling_frequency()
+num_chan = recording.get_num_channels()
+
+
+print('Channel ids:', channel_ids)
+print('Sampling frequency:', fs)
+print('Number of channels:', num_chan)
+
+
+#!cat tetrode9.prb #Asks for prb file
+# os.system('cat /home/adrian/Documents/SpikeSorting/Adrian_test_data/Irene_data/test_without_zero_main_channels/Tetrode_9_CH/tetrode9.prb') 
+recording_prb = recording.load_probe_file('tetrode.prb')
+
+print('Channels after loading the probe file:', recording_prb.get_channel_ids())
+print('Channel groups after loading the probe file:', recording_prb.get_channel_groups())
+
+#For testing only: Reduce recording.
+#recording_prb = se.SubRecordingExtractor(recording_prb, start_frame=100*fs, end_frame=420*fs)
+
+
+#Bandpass filter 
+recording_cmr = st.preprocessing.bandpass_filter(recording_prb, freq_min=300, freq_max=6000)
+recording_cache = se.CacheRecordingExtractor(recording_cmr); 
+
+
+
 
 Sorters2Compare=[];
 Sorters2CompareLabel=['KL','IC','Waveclus','HS','MS4','SC','TRI'];
@@ -60,6 +88,12 @@ plt.savefig('consensus_curation_spikes.pdf', bbox_inches='tight');
 plt.savefig('consensus_curation_spikes.png', bbox_inches='tight');
 plt.close()
 
+#Consider at least 2 sorters agreeing.
+agreement_sorting=mcmp.get_agreement_sorting(minimum_agreement_count=2);
+
+st.postprocessing.export_to_phy(recording_cache, 
+                                agreement_sorting, output_folder='phy_AGT_post',
+                                grouping_property='group', verbose=True, recompute_info=True)
     
 print('Consensus ended. Results saved')
 
