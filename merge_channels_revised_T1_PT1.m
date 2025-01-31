f847bb395387cfe0927572041b535dc4bcc0eab2
Adrián Alemán-Zapata <jraleman10@gmail.com>
2025-01-30 19:26:28 -0600

    Update merge_channels_revised_T1_PT1.m

--- merge_channels_revised_T1_PT1.m
+++ merge_channels_revised_T1_PT1.m
@@ -4,7 +4,7 @@ addpath(genpath('/home/genzel/dimitris/ADRITOOLS-master'))
 addpath(genpath('/home/genzel/dimitris/analysis-tools-master'))
 
 %Sampling freq:
-fs=20000; % For OS-normal Rats 1-9 this should be 20000.
+%fs=20000; % For OS-normal Rats 1-9 this should be 20000.
           % For other Rats of OS-normal and all RGS14 Rats use 30000.
 %%
 %Get trial folders 
@@ -94,7 +94,8 @@ for j=1:length(channels)
             file=file{1};
             
             if  j==1   %Only run with the first channel.
-                signal=load_open_ephys_data(file); % changed to simple version
+                [signal,~,info]=load_open_ephys_data(file); % changed to simple version
+                fs=info.header.sampleRate;                
                 trial_durations(1)=length(signal);
             end
             
